1d678531aac34e5b39c4dbc1c8bf5848bba15e04
bfpronaga <ronaga@bridgesforpeace.com>
2025-05-06 15:17:39 +0300

    esm external

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    experimental: {
+        esmExternals: false // Disable ESM external resolution
+    }
+    /* config options here */
 };
 
 export default nextConfig;
